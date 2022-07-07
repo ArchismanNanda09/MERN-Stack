f5ebf553d17f89b43bd2e0930af1cc6b03e6aa80
Archisman Nanda <74260865+ArchismanNanda09@users.noreply.github.com>
2022-07-07 21:56:21 +0530

    Create CategoryApi.js

---
+++ Client/src/Api/CategoryApi.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios'
+function CategoriesAPI(){
+  const [categories, setCategories] = useState([]);
+  const [callback, setCallback] = useEffect(false);
+  
+  
+  useEffect()=>{
+    const getCategories = async=()=>{
+      
+      
+    }
+  }
+
+}
